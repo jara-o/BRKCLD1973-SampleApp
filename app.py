e3709456748e618660fb190713821aeefeace82b
josterfe <jarao@web.de>
2023-01-22 15:25:58 +0100

    upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+
+if __name__ == '__main__':
+   app.run()
